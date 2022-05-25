cdfa31fc54adf1f237246b82bb4bb8de01ea90b0
Jason Ohanaga <johanaga@ucsc.edu>
2022-05-24 19:43:19 -0700

    changed navigation from feed to schedule after

--- Client/src/Components/AccountSetup/AccountSetup.js
+++ Client/src/Components/AccountSetup/AccountSetup.js
@@ -72,7 +72,7 @@ export default function AccountSetup() {
           if (res.data) {
             //    console.log(userType);
             setUserObject(res.data);
-            navigate('/feed');
+            navigate('/schedule');
           }
         });
     }
