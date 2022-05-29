339ccd40a75a622f356bc82b2df9ccf0be5e6f0b
Andre Assadi <assadiandre@gmail.com>
2022-05-29 14:03:59 -0700

    remove test files

--- Client/src/app/AuthenticatedApp.js
+++ Client/src/app/AuthenticatedApp.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import AccountSetup from '../Components/AccountSetup/AccountSetup';
 import Schedule from '../Components/Schedule/Schedule';
-import EndpointTest from '../Components/EndpointTest/EndpointTest';
 import FeedPage from '../Components/FeedPage/FeedPage';
 import ProfilePage from '../Components/ProfilePage/ProfilePage';
 import RedirectSetupFeed from './RedirectSetupFeed';
@@ -12,9 +11,7 @@ export default function AuthenticatedApp() {
       <Route path="/setup" element={<AccountSetup />} />
       <Route path="/schedule" element={<Schedule />} />
       <Route path="/feed" element={<FeedPage />} />
-      <Route path="/test" element={<EndpointTest />} />
       <Route path="/profile" element={<ProfilePage />} />
-      <Route path="/feedfilltest" element={<EndpointTest />} />
       <Route path="*" element={<RedirectSetupFeed />} />
     </Routes>
   );
